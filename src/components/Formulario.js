24115796ca145d36164696f9a07187502f6b06df
Catalina Carreto <isccatalinacarreto27@gmail.com>
2020-01-17 17:12:34 -0600

    feat: Agregar State en formulario

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,6 +1,21 @@
-import React, {Fragment} from 'react';
+import React, {Fragment, useState } from 'react';
 
 const Formulario = () => {
+
+    //Crear State de Citas
+    const [cita,actualizarCita] = useState({
+        mascota: '',
+        propietario: '',
+        fecha: '',
+        hora: '',
+        sintomas: ''
+    });
+
+    //Funcion que se ejecuta cada que el usuario escribe en un input
+    const actualizarState = () => {
+        console.log('escribiendo...');
+    }
+
     return ( 
         <Fragment>
             <h2>Crear Cita</h2>
@@ -12,6 +27,7 @@ const Formulario = () => {
                     name="mascota"
                     className="u-full-width"
                     placeholder="Nombre de la Mascota"
+                    onChange={actualizarState}
                 />
                 <label>Nombre del Dueño</label>
                 <input 
@@ -19,23 +35,27 @@ const Formulario = () => {
                     name="propietario"
                     className="u-full-width"
                     placeholder="Nombre del dueño de la mascota"
+                    onChange={actualizarState}
                 />
                 <label>Fecha</label>
                 <input 
                     type="date"
                     name="fecha"
                     className="u-full-width"
+                    onChange={actualizarState}
                 />
                 <label>Hora</label>
                 <input 
                     type="time"
                     name="hora"
                     className="u-full-width"
+                    onChange={actualizarState}
                 />
                 <label>Sintomas</label>
                 <input 
                     className="u-full-width"
                     name="sintomas"
+                    onChange={actualizarState}
                 />
                 <button
                     type="submit"
