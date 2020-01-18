21d492defcedb098f499e82b0d112fcc39f262ac
Catalina Carreto <isccatalinacarreto27@gmail.com>
2020-01-17 18:12:30 -0600

    feat: Validar formulario

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -11,6 +11,9 @@ const Formulario = () => {
         sintomas: ''
     });
 
+    //Crear State de Error
+    const [error, actualizarError]= useState(false)
+
     //Funcion que se ejecuta cada que el usuario escribe en un input
     const actualizarState = e => {
         actualizarCita({
@@ -25,12 +28,29 @@ const Formulario = () => {
     //Cuando el usuario presiona agregar cita
     const submitCita = e =>{
         e.preventDefault();
-        console.log('enviando form')
+
+        //Validar
+        if (mascota.trim() === '' || propietario.trim() === '' || fecha.trim() === '' || 
+            hora.trim() === '' || sintomas.trim() === '') {
+            actualizarError(true);
+            return;
+        }
+
+        //Asignar un ID
+
+        //Crear la cita
+
+        //Reiniciar el form
+
     }
 
     return ( 
         <Fragment>
             <h2>Crear Cita</h2>
+
+            {error 
+            ? <p className="alerta-error">Todos los campos son obligatorios</p>  
+            : null }
 
             <form
                 onSubmit={submitCita}
