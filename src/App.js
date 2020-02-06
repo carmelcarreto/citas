e1c71afcb65d8718278c8ed0f609bb634e9d36e1
Catalina Carreto <isccatalinacarreto27@gmail.com>
2020-02-05 21:00:29 -0600

    feat: Mostrar Cita

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, {Fragment, useState} from 'react';
 import Formulario from './components/Formulario';
 
+import Cita from './components/Cita';
+
+
 function App() {
 
   //Arreglo de citas
@@ -23,7 +26,13 @@ function App() {
               />
             </div>
             <div className="one-half column">
-              2
+              <h2>Administra tus citas</h2>
+              {citas.map(cita => (
+                <Cita 
+                  key={cita.id}
+                  cita={cita}
+                />
+              ))}
             </div>
           </div>
       </div>
